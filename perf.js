67be162c49923f60384172ae326fe05de53bad62
unknown <neilm813@gmail.com>
2019-10-19 17:25:35 -0700

    refact

--- perf.js
+++ perf.js
@@ -23,11 +23,13 @@ setRuntime(funcs[0]);
 setRuntime(funcs[1]);
 
 const fasterFuncIdx = funcs[0].runtime < funcs[1].runtime ? 0 : 1;
+const fasterFuncName = funcs[fasterFuncIdx].name || fasterFuncIdx;
 const slowerFuncIdx = 1 - fasterFuncIdx;
+const slowerFuncName = funcs[slowerFuncIdx].name || slowerFuncIdx;
 const runtimePctDiff = Math.round(100 - (funcs[fasterFuncIdx].runtime / funcs[slowerFuncIdx].runtime) * 100);
 
 console.table({
-	[funcs[fasterFuncIdx].name]: `${funcs[fasterFuncIdx].runtime} ms`,
-	[funcs[slowerFuncIdx].name]: `${funcs[slowerFuncIdx].runtime} ms`,
-	[funcs[fasterFuncIdx].name + " is faster than " + funcs[slowerFuncIdx].name + " by"]: `${runtimePctDiff}%`
+	[fasterFuncName]: `${funcs[fasterFuncIdx].runtime} ms`,
+	[slowerFuncName]: `${funcs[slowerFuncIdx].runtime} ms`,
+	[`${fasterFuncIdx} is faster than ${slowerFuncName} by`]: `${runtimePctDiff}%`
 });
