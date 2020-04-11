11a1bade010e2a5b9d212da84cae2c239d96483f
Neil M <neilm813@gmail.com>
2020-04-11 14:35:58 -0700

    comments

--- perfTemplate.js
+++ perfTemplate.js
@@ -1,6 +1,7 @@
 const funcs = [];
 
-const iterations = 10000;
+// small differences may become larger with more iterations
+const iterations = 1000;
 const args = []; // args to pass to both functions
 
 // paste functions here
@@ -9,14 +10,14 @@ funcs[0] = Function;
 funcs[1] = Function;
 
 function setRuntime(cb) {
+  const start = window.performance.now();
 
-	const start = window.performance.now();
+  for (let i = 0; i < iterations; i++) {
+    cb(...args);
+  }
 
-	for (let i = 0; i < iterations; i++)
-		cb(...args);
-
-	const end = window.performance.now();
-	cb.runtime = Math.round(end - start);
+  const end = window.performance.now();
+  cb.runtime = Math.round(end - start);
 }
 
 setRuntime(funcs[0]);
@@ -26,10 +27,12 @@ const fasterFuncIdx = funcs[0].runtime < funcs[1].runtime ? 0 : 1;
 const fasterFuncName = funcs[fasterFuncIdx].name || fasterFuncIdx;
 const slowerFuncIdx = 1 - fasterFuncIdx;
 const slowerFuncName = funcs[slowerFuncIdx].name || slowerFuncIdx;
-const runtimePctDiff = Math.round(100 - (funcs[fasterFuncIdx].runtime / funcs[slowerFuncIdx].runtime) * 100);
+const runtimePctDiff = Math.round(
+  100 - (funcs[fasterFuncIdx].runtime / funcs[slowerFuncIdx].runtime) * 100
+);
 
 console.table({
-	[fasterFuncName]: `${funcs[fasterFuncIdx].runtime} ms`,
-	[slowerFuncName]: `${funcs[slowerFuncIdx].runtime} ms`,
-	[`${fasterFuncIdx} is faster than ${slowerFuncName} by`]: `${runtimePctDiff}%`
+  [fasterFuncName]: `${funcs[fasterFuncIdx].runtime} ms`,
+  [slowerFuncName]: `${funcs[slowerFuncIdx].runtime} ms`,
+  [`${fasterFuncName} is faster than ${slowerFuncName} by`]: `${runtimePctDiff}%`,
 });
